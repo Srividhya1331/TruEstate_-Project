744a5dff2aacc90ca312545888c1391b074fcc52
Srividhya1331 <165308644+Srividhya1331@users.noreply.github.com>
2025-12-09 11:30:10 +0530

    Add base path for GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    base: "/TruEstate_-Project/",
   plugins: [react()],
   server: {
     port: 5173,
@@ -10,4 +11,5 @@ export default defineConfig({
     }
   }
 })
+
 
